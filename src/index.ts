0b1fd64c562aaaf29ce244a7790a377e04c691fb
George Cht <2hardforutoo@hotmail.gr>
2024-10-15 17:28:56 +0300

    docs(@jsr): added module tags with example for better https://jsr.io documentation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
+/**
+ * @module
+ *
+ * Type safe, framework agnostic, Zod based, custom events extension library.
+ *
+ * @example
+ * ```ts
+ * import { z } from "zod";
+ * import { EventController } from "zod-custom-events";
+ *
+ * const schema = z.object({
+ *   name: z.string(),
+ * });
+ *
+ * const controller = new EventController(schema, "myEvent", {
+ *   onError: ({ error }) => console.error("Validation error:", error),
+ *   onDispatch: ({ payload }) => console.log("Dispatching event with payload:", payload),
+ * });
+ * ```
+ */
 export * from './core'
 export type { Middleware } from './middleware'
