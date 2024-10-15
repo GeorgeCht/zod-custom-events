32d49b8d2ebcd636fc8633594a218efe660869a7
George Cht <2hardforutoo@hotmail.gr>
2024-10-15 19:31:11 +0300

    build(release-minor): added a minified build via rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,18 @@
-import typescript from "@rollup/plugin-typescript";
+import typescript from '@rollup/plugin-typescript'
+import { minify } from 'rollup-plugin-esbuild-minify'
 
 const typescriptOptions = {
   outputToFilesystem: false,
-  exclude: ["tests/**/*", "scripts/**/*"],
-};
+  exclude: ['tests/**/*', 'scripts/**/*'],
+}
 
 export default [
   {
-    external: ["zod"],
-    input: "src/index.ts",
+    external: ['zod'],
+    input: 'src/index.ts',
     output: {
-      file: "dist/index.js",
-      format: "esm",
+      file: 'dist/index.js',
+      format: 'esm',
     },
     treeshake: {
       annotations: true,
@@ -19,15 +20,29 @@ export default [
     plugins: [typescript(typescriptOptions)],
   },
   {
-    external: ["zod"],
-    input: "src/index.ts",
+    external: ['zod'],
+    input: 'src/index.ts',
     output: {
-      file: "dist/index.cjs",
-      format: "cjs",
+      file: 'dist/index.cjs',
+      format: 'cjs',
     },
     treeshake: {
       annotations: true,
     },
     plugins: [typescript(typescriptOptions)],
   },
-];
+  {
+    external: ['zod'],
+    input: 'src/index.ts',
+    output: {
+      name: 'index',
+      file: 'dist/index.min.js',
+      format: 'iife',
+      sourcemap: true,
+    },
+    plugins: [
+      typescript(typescriptOptions),
+      minify({ logLevel: 'debug', logLimit: 100 }),
+    ],
+  },
+]
